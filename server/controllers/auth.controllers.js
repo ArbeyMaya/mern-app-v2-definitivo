e4d10721e25f30c66761cc0ce6ff2432e0aa9ebe
Arbey Maya <vilmarbey@gmail.com>
2024-07-10 21:57:31 -0500

    pagina 81, funciona todo correcto, ahora a empezar a encriptar

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,5 +1,11 @@
+const User = require("../models/user.model.js")
+
 function register(req, res){
-    console.log("Se ha ejecutado el registro")
+    const {firstname, lastname, email, password} = req.body
+    console.log(req.body)
+
+    if(!email) res.status(400).send({msg: "El email es obligatorio"})
+    if(!password) res.status(400).send({msg: "La contraseña es obligatoria"})
 
     res.status(200).send({msg:"Funciono perecto !"})
 }
